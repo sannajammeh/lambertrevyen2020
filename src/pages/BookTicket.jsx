e529ac3f4f4fd270bd6233bae2def96379af486c
Sanna Jammeh <sannajam@outlook.com>
2020-02-11 15:18:25 +0100

    Improved imputs, added form State on BookTicket component

--- src/pages/BookTicket.jsx
+++ src/pages/BookTicket.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
 import { Title } from '../components/Title/Title';
 import { Input, IconInput } from '../components/Input/Input';
-import { FaMailBulk } from 'react-icons/fa';
+import { FaMailBulk, FaUser, FaPhone } from 'react-icons/fa';
 import { PlaysContext } from '../context/plays';
 
 const BookTicket = ({ match }) => {
@@ -12,20 +12,62 @@ const BookTicket = ({ match }) => {
 
   const selectedPlay = plays[id];
 
-  if (Object.keys(plays).length && !selectedPlay) {
-    return <Redirect to="/bestill" />;
-  }
-
   const { dateField } = selectedPlay || {};
+  // Handle Form
+  const initialState = {
+    name: '',
+    email: '',
+    phone: ''
+  };
+  const [formData, setFormData] = React.useState(initialState);
+  const { name, email, phone } = formData;
 
+  // No URL param?
+  if (Object.keys(plays).length && !selectedPlay) {
+    return <Redirect to='/bestill' />;
+  }
+  const handleChange = e => {
+    const { value, name } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <div className="container">
-      <Title size="md">{dateField} </Title>
-      <IconInput icon={FaMailBulk} placeholder="Epost Addresse" />
-      <Input placeholder="Navn" />
-      <Input placeholder="Navn" />
-      <Input placeholder="Navn" />
-      <Input placeholder="Navn" />
+    <div className='container BookTicket'>
+      <Title className='text-center' size='md'>
+        {dateField}
+      </Title>
+      <form onSubmit={handleSubmit}>
+        <IconInput
+          type='text'
+          name='name'
+          icon={FaUser}
+          placeholder='Navn'
+          value={name}
+          onChange={handleChange}
+        />
+        <IconInput
+          type='email'
+          name='email'
+          icon={FaMailBulk}
+          placeholder='Epost Addresse'
+          value={email}
+          onChange={handleChange}
+        />
+        <IconInput
+          icon={FaPhone}
+          type='number'
+          name='phone'
+          placeholder='Telefon'
+          value={phone}
+          onChange={handleChange}
+        />
+        <button className='button button-brand u-full-width' type='submit'>
+          Reserver billett
+        </button>
+      </form>
     </div>
   );
 };
