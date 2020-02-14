81ce7169ba08bbc40c566031a6d7e6dcfafae2e9
Sanna Jammeh <sannajam@outlook.com>
2020-02-14 12:42:54 +0100

    Configured cors. Added API Url to Frontend

--- client/src/pages/BookTicket.jsx
+++ client/src/pages/BookTicket.jsx
@@ -4,18 +4,28 @@ import PropTypes from 'prop-types';
 import { withRouter, Redirect } from 'react-router-dom';
 import TicketData from '../components/TicketData/TicketData';
 // Error types
-import { PLAY_IS_BOOKED, PLAY_NOT_ENOUGH_SEATS, VALIDATION_ERROR } from '../utils/error.types';
+import {
+  PLAY_IS_BOOKED,
+  PLAY_NOT_ENOUGH_SEATS,
+  VALIDATION_ERROR
+} from '../utils/error.types';
 // Redux
 import { createStructuredSelector } from 'reselect';
-import { selectPlays, selectPlaysFetching } from '../redux/plays/plays.selectors';
+import {
+  selectPlays,
+  selectPlaysFetching
+} from '../redux/plays/plays.selectors';
 import { connect } from 'react-redux';
 import {
   bookTicketStart,
   bookTicketSuccess,
   bookTicketFailure,
-  clearBookingErrors,
+  clearBookingErrors
 } from '../redux/tickets/tickets.actions';
-import { selectTicketLoading, selectTicketErr } from '../redux/tickets/tickets.selectors';
+import {
+  selectTicketLoading,
+  selectTicketErr
+} from '../redux/tickets/tickets.selectors';
 import BookingForm from '../components/BookingForm/BookingForm';
 import spinner from '../components/Spinner/Spinner';
 
@@ -34,7 +44,7 @@ const BookTicket = ({
   isFetching,
   isTicketFetching,
   formErrors,
-  history,
+  history
 }) => {
   const { id } = match.params;
 
@@ -50,11 +60,11 @@ const BookTicket = ({
     phone: '',
     price: '',
     seats: {},
-    ...formErrors,
+    ...formErrors
   };
   const noErrState = {
     errFields: {},
-    error: false,
+    error: false
   };
 
   const [formData, setFormData] = React.useState(initialState);
@@ -76,7 +86,11 @@ const BookTicket = ({
   const handleSelectChange = e => {
     const { value, name } = e.target;
     const { seats } = formData;
-    setFormData({ ...formData, seats: { ...seats, [name]: value }, ...noErrState });
+    setFormData({
+      ...formData,
+      seats: { ...seats, [name]: value },
+      ...noErrState
+    });
   };
 
   const handleSubmit = async e => {
@@ -87,7 +101,7 @@ const BookTicket = ({
     if (email !== emailConfirm)
       return setFormData({
         ...formData,
-        errFields: { ...errFields, email: 'Epost er ikke lik' },
+        errFields: { ...errFields, email: 'Epost er ikke lik' }
       });
 
     const requestData = {
@@ -96,12 +110,12 @@ const BookTicket = ({
       phone,
       playId: selectedPlay.id,
       date: selectedPlay.dateField,
-      seats,
+      seats
     };
     const requestObj = {
-      url: 'http://localhost:5000/api/tickets/',
+      url: 'https://api.lambertrevyen2020.no/v1/tickets',
       method: 'POST',
-      data: requestData,
+      data: requestData
     };
 
     try {
@@ -126,36 +140,41 @@ const BookTicket = ({
             fields.forEach(({ name }) => {
               errFields[name] = true;
             });
-            return bookTicketFailure({ errFields, message: 'Reservasjon avvist, sjekk felter.' });
+            return bookTicketFailure({
+              errFields,
+              message: 'Reservasjon avvist, sjekk felter.'
+            });
 
           case PLAY_NOT_ENOUGH_SEATS:
             return bookTicketFailure({
               message: `Det er kun ${maxSeats - Number(count)} ${
                 Number(count) > 1 ? 'sete' : 'seter'
-              } igjen.`,
+              } igjen.`
             });
+
           case PLAY_IS_BOOKED:
             return bookTicketFailure({ message: 'Forestilling er utsolgt.' });
+
           default:
             return bookTicketFailure({
-              message: 'En feil oppstod under bestillingen',
+              message: 'En feil oppstod under bestillingen'
             });
         }
       }
       // Undefined Error
       return bookTicketFailure({
-        message: 'En feil oppstod under bestillingen',
+        message: 'En feil oppstod under bestillingen'
       });
     }
   };
 
   // No URL param
   if (Object.keys(plays).length && !selectedPlay) {
-    return <Redirect to="/bestill" />;
+    return <Redirect to='/bestill' />;
   }
 
   return (
-    <div className="container BookTicket">
+    <div className='container BookTicket'>
       <PlayDataMemo
         seats={maxSeats - count}
         isLoading={isFetching}
@@ -166,7 +185,15 @@ const BookTicket = ({
       />
       <SpinnerBookingForm
         isLoading={isTicketFetching}
-        {...{ handleChange, handleSelectChange, handleSubmit, prices, formData, count, maxSeats }}
+        {...{
+          handleChange,
+          handleSelectChange,
+          handleSubmit,
+          prices,
+          formData,
+          count,
+          maxSeats
+        }}
       />
     </div>
   );
@@ -175,21 +202,21 @@ const BookTicket = ({
 BookTicket.propTypes = {
   match: PropTypes.object.isRequired,
   plays: PropTypes.object.isRequired,
-  isFetching: PropTypes.bool,
+  isFetching: PropTypes.bool
 };
 
 const mapStateToProps = createStructuredSelector({
   plays: selectPlays,
   isFetching: selectPlaysFetching,
   formErrors: selectTicketErr,
-  isTicketFetching: selectTicketLoading,
+  isTicketFetching: selectTicketLoading
 });
 
 const actions = {
   bookTicketStart,
   bookTicketSuccess,
   bookTicketFailure,
-  clearBookingErrors,
+  clearBookingErrors
 };
 
 export default withRouter(connect(mapStateToProps, actions)(BookTicket));
--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,15 @@ import cors from 'cors';
 // Routes
 import Api from './Routes/api';
 
+const corsOptions = {
+  origin: 'https://lambertrevyen2020.no'
+};
+
 export function server(dep) {
   const app = express();
   // Middlewares
+  app.use(cors(corsOptions));
   app.use(express.json());
-  app.use(cors());
 
   // Routes
   app.use('/v1', Api);
